f45c9b8a8dc6e69436eaa3adf0831e2000bfbe12
tingusers <2119038719@qq.com>
2020-04-11 14:41:15 +0800

    dev合并到buys分支

---
+++ src/App_BASE_243.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <TabBar v-if="$route.meta.tabBar"/>
+    <div>变更内容</div>
+  </div>
+</template>
+<script>
+import TabBar from 'common/tabBar';
+export default {
+  name:'App',
+  components: {
+    TabBar
+    }
+  }
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/App_LOCAL_243.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <TabBar v-if="$route.meta.tabBar"/>
+    <div>回滚后修改文件</div>
+  </div>
+</template>
+<script>
+import TabBar from 'common/tabBar';
+export default {
+  name:'App',
+  components: {
+    TabBar
+    }
+  }
+
+</script>
+
+<style lang="scss">
+
+</style>
